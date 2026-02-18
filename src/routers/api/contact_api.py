2e1d79377e4b6e36100515085b28a5a8d4d714fa
Tanmaya Kumar Naik <tanmayakumarnaik2003@gmail.com>
2026-02-18 15:21:52 +0530

    feat: created reading contacts api by admin only.
    - added the dependency of the admin.

--- src/routers/api/contact_api.py
+++ src/routers/api/contact_api.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Annotated, List
 from src.schemas import contact_schemas
+from src.admin.auth import require_admin
 
 router = APIRouter(
     prefix="/api",
@@ -25,11 +26,20 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 # response_model=List[contact_schemas.ContactResponse]
 @router.get("/read-all-contacts", status_code=status.HTTP_200_OK)
-async def read_all_contacts(db: db_dependency):
+async def read_all_contacts(db: db_dependency, _: str =  Depends(require_admin)):
     return db.query(Contact).all()
+
+@router.get("/read-the-contact/{contact_id}", status_code=status.HTTP_200_OK)
+async def read_a_contact(db: db_dependency, contact_id: int = Path(gt=0), _: str =  Depends(require_admin)):
+    contact = db.query(Contact).filter(Contact.sno == contact_id).first()
+
+    if contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found.")
+
+    return contact
     
 @router.get("/read-all-contacts-paginated", status_code=status.HTTP_200_OK)
-async def read_all_contacts_paginated(db: db_dependency, page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=100)):
+async def read_all_contacts_paginated(db: db_dependency, page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=100), _: str =  Depends(require_admin)):
     skip = (page - 1) * size
     total = db.query(Contact).count()
     contacts_array = (
