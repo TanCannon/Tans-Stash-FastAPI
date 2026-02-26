6404633affbceddd95d92f2e2c2601f204b42a4e
Tanmaya Kumar Naik <tanmayakumarnaik2003@gmail.com>
2026-02-27 00:39:11 +0530

    refactor: moved database url into env file.
    - So that I can change it in EC2 without getting commited.

--- src/database.py
+++ src/database.py
@@ -3,8 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from src.core.settings import settings
+
 #path and name of the location when you need database
-SQLACHEMY_DATABASE_URL = 'sqlite:///./pyconnectivity.db'
+SQLACHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLACHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
